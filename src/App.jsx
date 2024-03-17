c4c04cf939050e3a11f3e1c5b1fa70dafd8127f3
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2024-03-17 23:51:03 +0600

    Add base font styles. Add base colors. Create base layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import './index.css';
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Ayy Kori</h1>
-    </>
+    <div className="grid grid-cols-[2fr_9fr_3fr] min-h-screen font-barlow ">
+      <aside className="bg-red-300 ">Aside</aside>
+      <main className="bg-green-300">Main</main>
+      <aside className=" bg-sky-300">Aside</aside>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: ['"Barlow"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        text: '#060709',
+        grey: '#7B7B7B',
+        primary: '#11998E',
+        shade: '#B9F2E5',
+        'linear-red': '#FF6A49',
+        'linear-yellow': '#E78860',
+        'linear-blue': '#61E7D3',
+        background: '#E0EDEA',
+      },
+    },
   },
   plugins: [],
 };
